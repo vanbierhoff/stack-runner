6bcc2a1c072d2e5a1b63a553cf413970daed171d
hoff <ie@fv.ee>
2024-09-19 09:58:24 +0300

    add handle error

--- src/lib/v-stack-runner.spec.ts
+++ src/lib/v-stack-runner.spec.ts
@@ -1,7 +1,43 @@
-import { vStackRunner } from './v-stack-runner';
+import { BaseScheduler, Stack } from '@v/stack-runner';
+import { expect, TaskContext, TestContext } from 'vitest';
 
-describe('vStackRunner', () => {
+
+describe('Stack tets', () => {
   it('should work', () => {
-    expect(vStackRunner()).toEqual('v-stack-runner');
+    const testFn = () => {
+      throw new Error('test');
+    };
+
+    const runner = new BaseScheduler('sync');
+    const stack = new Stack(runner, true);
+    stack.add(testFn, (result) => {
+      expect(result).toBeInstanceOf(Error);
+    });
+  });
+
+  it('should number', () => {
+    const testFn = () => {
+      return 10;
+    };
+
+    const runner = new BaseScheduler('sync');
+    const stack = new Stack(runner, true);
+    stack.add(testFn, (result) => {
+      expect(result).toBeTypeOf('number');
+    });
+  });
+
+
+  it('should number from promise', async (done: TaskContext & TestContext) => {
+    const testFn = async () => {
+      return 5;
+    };
+
+    const runner = new BaseScheduler('sync');
+    const stack = new Stack(runner, true);
+    stack.add(testFn, (result) => {
+      console.log('NUMBER WAIT ASYNC', result);
+      expect('result').toEqual('string');
+    });
   });
 });
